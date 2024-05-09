2f6615ef231d21331fdaff1413c2d6563218f071
Simon Nair <snair205@icloud.com>
2024-05-09 16:16:54 -0400

    Patient 0 is random markhov chains returns patient_0 started histogram for expirement, currently broken

--- weekly_notebooks/MarkhovChain.R
+++ weekly_notebooks/MarkhovChain.R
@@ -4,18 +4,19 @@ library(gridExtra)  # install.packages("gridExtra")
 
 markhov_virus <- function(end_time, beta, gamma, S, I, R = 0){
   #output value as: "time_of_event: infected/0, infected/recovered, 
-  N = S + I + R
-  population = seq(1, N, by=1)
-  I_list = population[1:I]
-  S_list = population[I:S+I]
-  R_list = population[S+I:N]
-  table = matrix(nrow=0, ncol=6)
-  colnames(table) <- c("time", "infector", "affected", "S", "I", "R")
-  current_time = 0
-  infecter = 0
-  affected = 0
-  table <- rbind(table, c(0,0,0,S,I,R))
-  while (current_time < end_time & I > 0 & (gamma > 0 | I < N)){
+  N <- S + I + R
+  population <- 1:N
+  p_0 = sample(population, 1)
+  I_list <- list(p_0)
+  S_list <- population[-p_0]
+  R_list <- c()
+  table <- matrix(nrow=0, ncol=7)
+  colnames(table) <- c("time", "infector", "affected", "S", "I", "R", "I List")
+  current_time <- 0
+  infecter <- 0
+  affected <- 0
+  table <- rbind(table, c(0,0,0,S,I,R, toString(I_list)))
+  while (current_time < end_time & I > 0 & (gamma > 0 || I < N)){
     next_time <- rexp(1, beta * S *I / N + gamma *I)
     current_time <- current_time + next_time
     U <- runif(1)
@@ -24,7 +25,7 @@ markhov_virus <- function(end_time, beta, gamma, S, I, R = 0){
       S = S - 1
       infecter <- sample(I_list, 1)
       affected <- sample(S_list, 1)
-      S_list <- S_list[S_list != affected]
+      S_list <- setdiff(S_list, c(affected))
       I_list <- c(I_list, affected)
     }
     else {
@@ -33,13 +34,20 @@ markhov_virus <- function(end_time, beta, gamma, S, I, R = 0){
       R = R + 1
       infecter <- sample(I_list, 1)
       affected <- 0
-      I_list <- I_list[I_list != infecter]
+      I_list <- setdiff(I_list, c(infecter))
       # S_list <- c(S_list, affected)
       R_list <- c(R_list, infecter)
     }
-    table <- rbind(table, c(current_time, infecter, affected, S, I, R))
+    table <- rbind(table, c(current_time, infecter, affected, S, I, R, toString(I_list)))
+    table <- data.frame(table)
+    table$time <- as.numeric(table$time)
   }
-  return(table[-nrow(table), ])
+  table <- table[-nrow(table), ]
+  return(list(table = table, p_0 = p_0))
 }
 
-data.frame(markhov_virus(end_time=3,beta=0.7, gamma=0, S=59, I=1))
+result <- markhov_virus(end_time=10,beta=0.7, gamma=0, S=59, I=1)
+table <- result$table
+p_0 <- result$p_0
+print(table[-7]) # not showing I list
+print(p_0)
