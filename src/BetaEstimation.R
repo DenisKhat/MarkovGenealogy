c97f38bb52970a3adedcac213e5d7821e8661b22
Decay <21dyk2@queensu.ca>
2024-05-13 12:41:48 -0400

    added method C

--- src/BetaEstimation.R
+++ src/BetaEstimation.R
@@ -7,7 +7,7 @@ methodA <- function(I, t, N){
 }
 
 
-table <- markhov_virus(60, 1, 0, 59, 1)
+table <- markhov_virus(10, 0.4, 0, 59, 1)
 combined <- list(times = table$time, I=table$I)
 df = as.data.frame(do.call(cbind, combined))
 
@@ -23,13 +23,26 @@ methodB <- function(data, N){
     }
     return(val)
   }
-  return(optimize(LB, interval=c(0,2), maximum = TRUE)$maximum)
+  return(optimize(LB, interval=c(0,1), maximum = TRUE)$maximum)
 }
 
-df = df[sample(nrow(df), 30), ]
+# df = df[sample(nrow(df), 30), ]
 # data <- data[order(do.call(rbind, data$times)),]
 # print(df)
 # df[[4,1]]
-methodB(df, 60)
+# methodB(df, 60)
 
-#df$times[i]
+methodC <- function(data, t_final,N){
+  #data must be incremented times. (corresponding to time of infections)
+  s_obs = c(0,diff(data))
+  M = length(data)
+  LC <- function(beta) {
+    lambda = sapply(seq(N), function(i) beta * (N-i) * i / N)
+    pdfs = sapply(seq(M), function(i) dexp(s_obs[i],lambda[i]))
+    p_M_last = 1 - pexp(t_final-data[M], lambda[M+1])
+    return(p_M_last * prod(as.vector(pdfs)))
+  }
+  return(optimize(LC, interval=c(0,1), maximum = TRUE)[["maximum"]])
+}
+df = as.vector(do.call(rbind, df[["times"]]))
+methodC(df, 10, 60)
