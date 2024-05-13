9e3e199eece1d5eddd9d5cb10df77199cb359264
Simon Nair <snair205@icloud.com>
2024-05-13 12:41:39 -0400

    graph method A

--- src/BetaEstimation.R
+++ src/BetaEstimation.R
@@ -1,35 +1,67 @@
 source('src/MarkhovChain.R')
 
+time = 10
+N = 60
 
-methodA <- function(I, t, N){
-  LA <- function(beta) markhov_probability(t, beta, 0, N-1, 1)[[I]]
-  return(optimize(LA, interval=c(0,1), maximum = TRUE)[["maximum"]])
-}
-
-
-table <- markhov_virus(60, 1, 0, 59, 1)
+table <- markhov_virus(60, 0.7, 0, 59, 1)
 combined <- list(times = table$time, I=table$I)
 df = as.data.frame(do.call(cbind, combined))
 
 
-methodB <- function(data, N){
-  data <- data[order(do.call(rbind, data$times)),]
-  row.names(data) <- NULL
-  print(data)
-  LB <- function(beta){
-    val = markhov_probability(data[[1,1]], beta, 0, N-1, 1)[ data[[1,2]] ]
-    for (i in 2:nrow(data)){
-      val = val * markhov_probability(data[[i,1]] - data[[i-1,1]], beta, 0, N-data[[i-1,2]], data[[i-1,2]])[data[[i,2]]]
-    }
-    return(val)
+I_at_t <- function(data, time){
+  index <- which(table$time == time)  # get index of desired time
+  if (length(index) == 0){ # if no event at exact time
+    index <- which(table$time == closest_lower(time, table$time)) # look at closest earlier event
   }
-  return(optimize(LB, interval=c(0,2), maximum = TRUE)$maximum)
+  # get the number of infected at time
+  I <- as.numeric(table$I[index])
+  return(I)
 }
 
-df = df[sample(nrow(df), 30), ]
-# data <- data[order(do.call(rbind, data$times)),]
-# print(df)
-# df[[4,1]]
-methodB(df, 60)
+I <- I_at_t(df, time)
+
+methodA <- function(I, t, N){
+  x <- seq(0, 1, by = 0.01)
+  y <- c()
+  LA <- function(beta) markhov_probability(t, beta, 0, N-1, 1)[[I]]
+  
+  for (b in x){
+    y <- c(y, LA(b))
+  }
+  # print(y)
+  spline_fun <- splinefun(x, y)
+  spline_interp <- data.frame(beta = x, LA = spline_fun(seq(0, 1, by = 0.01)))
+  # list_A <- list(beta = x, LA = y)
+  # df_A <- as.data.frame(do.call(cbind, list_A))
+  p <- ggplot(spline_interp, aes(x = beta, y = LA)) +
+      geom_line() +
+      xlab('B') +
+      ylab('L(B)')
+  print(p)
+
+  return(optimize(LA, interval=c(0,1), maximum = TRUE)[["maximum"]])
+}
+methodA(I, time, N)
+
+
+# methodB <- function(data, N){
+#   data <- data[order(do.call(rbind, data$times)),]
+#   row.names(data) <- NULL
+#   print(data)
+#   LB <- function(beta){
+#     val = markhov_probability(data[[1,1]], beta, 0, N-1, 1)[ data[[1,2]] ]
+#     for (i in 2:nrow(data)){
+#       val = val * markhov_probability(data[[i,1]] - data[[i-1,1]], beta, 0, N-data[[i-1,2]], data[[i-1,2]])[data[[i,2]]]
+#     }
+#     return(val)
+#   }
+#   return(optimize(LB, interval=c(0,2), maximum = TRUE)$maximum)
+# }
+# 
+# df = df[sample(nrow(df), 30), ]
+# # data <- data[order(do.call(rbind, data$times)),]
+# # print(df)
+# # df[[4,1]]
+# methodB(df, 60)
 
 #df$times[i]
