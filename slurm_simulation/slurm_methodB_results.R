456887b35fdf57b8c4c4af52ff336febb069157f
Simon Nair <snair205@icloud.com>
2024-05-28 15:48:29 -0400

    name change

--- slurm_simulation/slurm_methodB_results.R
+++ slurm_simulation/slurm_methodB_results.R
@@ -4,12 +4,12 @@ estimate <- mean(methodB_df$estimate)
 CI <- mean(methodB_df$inCI, na.rm = TRUE)
 width <- mean(methodB_df$width, na.rm = TRUE)
 tot_length <- length(methodB_df[,1])
-sims_w_events <- methodB_df[!is.na(methodB_df$inCI), ]
-length_sims_w_events <- length(sims_w_events[,1])
+sims_wout_outliers <- methodB_df[!is.na(methodB_df$inCI), ]
+length_sims_wout_outliers <- length(sims_w_events[,1])
 
 
 paste("avg estimate:", estimate)
 paste("CI:", CI)
 paste("avg width:", width)
 paste("num of total sims:", tot_length)
-paste("num of sims w events:", length_sims_w_events)
+paste("num of sims wout outliers:", length_sims_wout_outliers)
