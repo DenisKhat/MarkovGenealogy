62e64784707945262545270d2fdb0bd6d74c5dfc
Simon Nair <snair205@icloud.com>
2024-05-28 14:31:32 -0400

    method B slurm, added count for simulations minus the simulations where nothing happens

--- slurm_simulation/slurm_methodB_results.R
+++ slurm_simulation/slurm_methodB_results.R
@@ -3,9 +3,13 @@ methodB_df <- read.csv("slurm_simulation/combined.csv")
 estimate <- mean(methodB_df$estimate)
 CI <- mean(methodB_df$inCI, na.rm = TRUE)
 width <- mean(methodB_df$width, na.rm = TRUE)
-length <- length(methodB_df[,1])
+tot_length <- length(methodB_df[,1])
+sims_w_events <- methodB_df[!is.na(methodB_df$inCI), ]
+length_sims_w_events <- length(sims_w_events[,1])
+
 
 paste("avg estimate:", estimate)
 paste("CI:", CI)
 paste("avg width:", width)
-paste("num of sims:", length)
+paste("num of total sims:", tot_length)
+paste("num of sims w events:", length_sims_w_events)
