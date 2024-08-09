b465585560b3b9428693bf0fea17728da3dc7a55
Denis Khatnyuk <d.khatnyuk@gmail.com>
2024-08-09 03:06:38 +0000

    added experiment code. (it doesn't work, but worth a shot)

--- src/TroyTrees.R
+++ src/TroyTrees.R
@@ -88,7 +88,6 @@ get_indexing_table <- function(N){
   }
   return(df)
 }
-# prob <- as.numeric(prob)
 # prob
 # sum(prob)
 get_partial_info_bridge_pmf <- function(time, beta, gamma, initial, final, N){
@@ -213,55 +212,86 @@ partial_info_bridge_experiment <- function(time, beta, gamma, initial, final, N)
   chi <- function(I, L) {
     if(2 <= L & L <= I) return(choose(L, 2) / choose(I, 2))
     else return(0) }
+  xi <- function(I, L) {
+    return(L/I)
+  }
+  
+  STATES_COUNT <- (N+1)*(N+2)/2
   
   # P, the P(i, l | i_0, l_0) ---
-  C <- matrix(data=0, nrow=N+1, ncol=N+1)
-  for(k in 0:N){ # the index is infected + 1
-    C[k+1,k+1] <- - lambda(k) - mu(k)
-    if (k > 0) C[k+1, k] <- mu(k-1)
-    if (k < N) C[k+1, k+2] <- lambda(k+1)
-  }
-  initial_p <- sapply(0:N, function(x) 0 + (x == initial$I)) 
-  print(C)
-  forwards_P <- expm(C*(time-initial$time)) %*% initial_p #(A = C, v = initial_p, t=time-initial$time)$eAtv 
-  #print(forwards_P)
-  
-  # Q, the P(i_f, l_f | i, l) ---
-  STATES_COUNT <- (N+1)*(N+2)/2
-  B <- matrix(data=0, nrow=STATES_COUNT, ncol=STATES_COUNT)
+  # C <- matrix(data=0, nrow=N+1, ncol=N+1)
+  # for(k in 0:N){ # the index is infected + 1
+  #   C[k+1,k+1] <- - lambda(k) - mu(k)
+  #   if (k > 0) C[k+1, k] <- mu(k-1)
+  #   if (k < N) C[k+1, k+2] <- lambda(k+1)
+  # }
+  # 
+  A <- matrix(data=0, nrow=STATES_COUNT, ncol=STATES_COUNT)
   for(k in 1:STATES_COUNT){
     curr <- get_state(k)
     # Construct a matrix similar to that of above, using mu and lambda, 
     # (instead of up/down), and transposing.
-    B[k,k] <- -mu(curr$I+1) - lambda(curr$I-1)
+    if (curr$I < N){
+      A[k,k] <- A[k,k] - lambda(curr$I) * (1 - chi(curr$I + 1,curr$L) + chi(curr$I+1, curr$L+1))
+      A[k,get_index(curr$I+1, curr$L)] <- mu(curr$I+1)}
+    if (curr$L < curr$I){
+      A[k,get_index(curr$I-1, curr$L)] <- lambda(curr$I-1) *(1-chi(I=curr$I, L=curr$L))
+      A[k,k] <- A[k,k] -mu(curr$I)
+    }
+    if (curr$L >= 1){
+      A[k,get_index(curr$I-1, curr$L-1)] <- lambda(curr$I-1) * chi(I=curr$I, L=curr$L)
+    }
+  }
+  # print(A)
+  initial_p <- numeric(length = STATES_COUNT)
+  initial_p[get_index(i=initial$I, l=initial$L)] <- 1
+  forwards_P <- expm(A*(time-initial$time)) %*% initial_p #(A = C, v = initial_p, t=time-initial$time)$eAtv 
+  # print(sum(forwards_P))
+  # print(forwards_P)
+  
+  # Q, the P(i_f, l_f | i, l) ---
+  B <- matrix(data=0, nrow=STATES_COUNT, ncol=STATES_COUNT)
+  for(k in 1:STATES_COUNT){
+    curr <- get_state(k)
+    B[k,k] <- -lambda(curr$I-1)
     if (curr$L < curr$I){
       B[k,get_index(curr$I-1, curr$L)] <- mu(curr$I)
     }
     if (curr$I < N){
+      B[k,k] <- B[k,k] - mu(curr$I+1)
       B[k,get_index(curr$I+1, curr$L)] <- lambda(curr$I) * (1-chi(I=curr$I+1, L=curr$L))
       B[k,get_index(curr$I+1, curr$L+1)] <- lambda(curr$I) * chi(I=curr$I+1,L=curr$L+1)
     }
   }
   final_g <- numeric(length = STATES_COUNT)
   final_g[get_index(i=final$I, l=final$L)] <- 1
-  backwards_P <- expAtv(B, final_g, final$time - time)$eAtv
-  backwards_initial <- expAtv(B, final_g, final$time - initial$time)$eAtv
-  summed_indices <- sapply(0:initial$I, function(l) get_index(initial$I, l))
-  summed_indices <- as.integer(summed_indices)
-  normalization <- sum(backwards_initial[summed_indices])
+  backwards_P <- expm(B * (final$time - time)) %*% final_g
+  # print(B)
+  # print(sum(backwards_P))
+  ### Normalization Constant, P(i_f, l_f | i_i, l_i)
+  ### Try both ways for a sanity check.
+  k_i <- 3
+  k_f <- get_index(i = final$I, l=final$L)
+  # print((expm(B * final$time) %*% final_g)[k_i])
+  # print((expm(A * final$time) %*% initial_p)[k_f])
+  ### TRY SUMMING?
+  norm <- sum(forwards_P * backwards_P)
+  # print(norm)
+  
   final_out <- data.frame(I=c(), L=c(), P=c())
   for(k in 1:STATES_COUNT){
     curr <- get_state(k)
-    prob <- backwards_P[k] * forwards_P[curr$I + 1] / normalization
+    prob <- backwards_P[k] * forwards_P[k] / norm
     dta <- list(I=curr$I, L=curr$L, P=prob)
     final_out <- rbind(final_out, dta)
   }
-  #norm <- sum(final_out$P)
-  final_out$P <- final_out$P# / norm
   return(final_out)
 }
 
 
-prob <- partial_info_bridge_experiment(time=1, beta=0.7, gamma=0.3, initial=list(time=0,I=1),final=list(time=5, I=6, L=5), N=10)
-prob_2 <- get_partial_info_bridge_pmf(time=1, beta=0.7, gamma=0.3, initial=list(time=0,I=1),final=list(time=5, I=6, L=5), N=10)
-sum(prob$P)
+
+
+
+
+prob <- partial_info_bridge_experiment(time=3, beta=0.7, gamma=0.3, initial=list(time=0,I=1, L=1),final=list(time=5, I=6, L=5), N=10)
+prob_2 <- get_partial_info_bridge_pmf(time=3, beta=0.7, gamma=0.3, initial=list(time=0,I=1, L=1),final=list(time=5, I=6, L=5), N=10)
